da891cba34a8873487bcb1142b712d7a4c530a05
superlin <superlin1990@gmail.com>
2015-09-23 20:55:17 +0800

    add imge onerror event

--- html2canvas/js/html2canvas.js
+++ html2canvas/js/html2canvas.js
@@ -18,6 +18,7 @@ window.onload = function () {
       data =
         '<svg xmlns="http://www.w3.org/2000/svg" width="' + rect.width + '" height="' + rect.height + '">' +
           '<foreignObject width="100%" height="100%">' +
+            // se.serializeToString(document.getElementById('holder')) +
             se.serializeToString(document.documentElement) +
           '</foreignObject>' +
         '</svg>',
@@ -30,6 +31,9 @@ window.onload = function () {
       // draw svg into canvas
       ctx.drawImage(img, 0, 0);
       DOMURL.revokeObjectURL(url);
+    };
+    img.onerror = function () {
+      alert('error');
     };
     img.src = url;
 
