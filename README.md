ecf2683b17b33adff1c88547c0a936cd91b25326
superlin <superlin1990@gmail.com>
2015-09-23 20:31:17 +0800

    html2canvas base version

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # learn-canvas
 
-- [ ] 贪吃蛇（snake）
-- [ ] 打砖块（breakout）
-- [ ] 雷电（raiden）
-- [ ] html2canvas
+1. [ ] 贪吃蛇（snake）
+2. [ ] 打砖块（breakout）
+3. [ ] 雷电（raiden）
+4. [ ] html2canvas
+  - [ ] Retina屏幕适应问题（暂无解决方案，html2canvas库也没有解决）
+  - [ ] Safari浏览器无法使用该功能
---
+++ html2canvas/js/html2canvas.js
@@ -0,0 +1,39 @@
+/*jshint browser: true*/
+window.onload = function () {
+  var btn = document.getElementById("btn");
+  // listen click vent
+  btn.addEventListener('click', function () {
+    var canvas = document.createElement('canvas'),
+      ctx = canvas.getContext('2d'),
+      rect = document.documentElement.getBoundingClientRect();
+
+    // set canvas width and height
+    canvas.width = rect.width;
+    canvas.height = rect.height;
+
+    document.documentElement.appendChild(canvas);
+
+    // serialize html content and load it to an image
+    var se = new XMLSerializer(),
+      data =
+        '<svg xmlns="http://www.w3.org/2000/svg" width="' + rect.width + '" height="' + rect.height + '">' +
+          '<foreignObject width="100%" height="100%">' +
+            se.serializeToString(document.documentElement) +
+          '</foreignObject>' +
+        '</svg>',
+      svg = new Blob([data], { type: 'image/svg+xml;charset=utf-8' }),
+      DOMURL = window.URL || window.webkitURL || window,
+      url = DOMURL.createObjectURL(svg);
+
+    var img = new Image();
+    img.onload = function () {
+      // draw svg into canvas
+      ctx.drawImage(img, 0, 0);
+      DOMURL.revokeObjectURL(url);
+    };
+    img.src = url;
+
+    // button can only be clicked once
+    btn.removeEventListener('click', arguments.callee);
+  });
+};
